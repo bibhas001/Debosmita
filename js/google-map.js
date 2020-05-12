89dbecb5c25f0ee18a1b829868db3afc80dea16d
bibhas001 <42467014+bibhas001@users.noreply.github.com>
2020-05-12 17:00:44 +0530

    Revert "vvv"
    This reverts commit 3712307778e1da86f10ee4fcb219170c4ec65a21.

--- js/google-map.js
+++
@@ -1,7 +0,0 @@
-function myMap() {
-var mapProp= {
-    center:new google.maps.LatLng(55.508742,12.120850),
-    zoom:5,
-};
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
